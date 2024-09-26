eff0186b601d02e3067db387a27492dfa2ca13c8
redbeluga <kevinxu9455@gmail.com>
2024-09-26 02:48:13 -0400

    Combined relays with lobbies
    Handles host disconnect from fishnet
    Need to add checks for currentlobby polls if its null
    Create actual lobby ui and stuff

--- Shadow Chess/Assets/Script/Canvas/LobbyHudCanvas.cs
+++ Shadow Chess/Assets/Script/Canvas/LobbyHudCanvas.cs
@@ -1,31 +1,107 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FishNet.Managing;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyHudCanvas : MonoBehaviour
 {
-    private LobbyManager _lobbyManager;
-
     [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private GameObject _leaveLobbyButton; 
+    [SerializeField] private GameObject _createLobbyButton; 
+    [SerializeField] private GameObject _joinLobbyButton; 
+    [SerializeField] private GameObject _lobbyCodeInputField; 
+    [SerializeField] private GameObject _lobbyInfoTextObject;
+    // [SerializeField] private GameObject _lobbyPlayerInfoTextObject;
+    
+    private TMP_Text _lobbyInfoText;
+    private bool inLobby = false;
 
-    private void OnGUI()
+
+    private void Awake()
     {
-        _lobbyManager = FindObjectOfType<LobbyManager>();
+        _lobbyInfoText = _lobbyInfoTextObject.GetComponentInChildren<TMP_Text>();
     }
 
-    public void OnClickCreate()
+    private void Start()
     {
-        Debug.Log("Creating relay");
-        _lobbyManager.CreateRelay();
+        LobbyManager.Instance.OnLeaveLobby += OnLeaveLobby;
+        LobbyManager.Instance.OnJoinLobby += OnEnterLobby;
     }
 
-    public void OnClickJoin()
+    public async void OnClickCreate()
     {
-        Debug.Log("Joining relay: " + _inputField.text);
-        _lobbyManager.JoinRelay(_inputField.text);
+        Debug.Log("Creating lobby");
+        await LobbyManager.Instance.CreateLobby();
+    }
+
+    public async void OnClickJoin()
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(_inputField.text))
+            {
+                Debug.Log("Quick joining lobby");
+                await LobbyManager.Instance.QuickJoinLobby();
+            }
+            else
+            {
+                Debug.Log("Joining lobby: " + _inputField.text);
+                await LobbyManager.Instance.JoinLobby(_inputField.text);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+    
+    public async void OnClickLeave()
+    {
+        try
+        {
+            await LobbyManager.Instance.LeaveLobby();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public void OnEnterLobby(object sender, LobbyManager.LobbyEventArgs e)
+    {
+        Debug.Log("Changing Lobby Hud UI to Lobby");
         _inputField.text = "";
+        
+        inLobby = true;
+        _leaveLobbyButton.SetActive(true);
+        _lobbyInfoTextObject.SetActive(true);
+        // _lobbyPlayerInfoTextObject.SetActive(true);
+        
+        _createLobbyButton.SetActive(false);
+        _joinLobbyButton.SetActive(false);
+        _lobbyCodeInputField.SetActive(false);
+        
+        _lobbyInfoText.text = LobbyManager.Instance.getLobbyName();
+    }
+
+    public void OnLeaveLobby(object sender, LobbyManager.LobbyEventArgs e)
+    {
+        Debug.Log("Changing Lobby Hud UI to Find Lobbies");
+        inLobby = false;
+        _leaveLobbyButton.SetActive(false);
+        _lobbyInfoTextObject.SetActive(false);
+        // _lobbyPlayerInfoTextObject.SetActive(false);
+        
+        _createLobbyButton.SetActive(true);
+        _joinLobbyButton.SetActive(true);
+        _lobbyCodeInputField.SetActive(true);
+    }
+
+    public void UpdatePlayerList()
+    {
+        
     }
 }
--- Shadow Chess/Assets/Script/Networking/LobbyManager.cs
+++ Shadow Chess/Assets/Script/Networking/LobbyManager.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using FishNet.Managing;
-using FishNet.Managing.Transporting;
 using FishNet.Transporting.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
@@ -8,144 +11,320 @@ using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public enum EncryptionType
+{
+    DTLS, // Datagram Transport Layer Security
+    WSS // Web Socket Secure
+}
 
 public class LobbyManager : MonoBehaviour
 {
-    private Lobby hostLobby;
-    private float heartBeatTimer;
-    [SerializeField] private GameObject _networkManager;
+    [SerializeField] string lobbyName = "Lobby";
+    [SerializeField] int maxPlayers = 4;
+    [SerializeField] public NetworkManager _networkManager;
+    [SerializeField] EncryptionType encryption = EncryptionType.DTLS;
 
-    private async void Start()
+    public static LobbyManager Instance { get; private set; }
+
+    public string PlayerId { get; private set; }
+    public string PlayerName { get; private set; }
+    private bool isHost = false;
+    public string CurrentHostId { get; private set; }
+
+    Lobby currentLobby;
+    private string connectionType => encryption == EncryptionType.DTLS ? k_dtlsEncryption : k_wssEncryption;
+
+    const float k_lobbyHeartbeatInterval = 15f;
+    const float k_lobbyPollInterval = 1.1f;
+    const string k_keyJoinCode = "RelayJoinCode";
+    private const string k_hostID = "HostID";
+    const string k_dtlsEncryption = "dtls"; // Datagram Transport Layer Security
+    const string k_wssEncryption = "wss"; // Web Socket Secure, use for WebGL builds
+
+    public event EventHandler<LobbyEventArgs> OnLeaveLobby;
+    public event EventHandler<LobbyEventArgs> OnJoinLobby;
+    
+    public class LobbyEventArgs : EventArgs {
+        public Lobby lobby;
+    }
+
+    private void Awake()
     {
-        await UnityServices.InitializeAsync();
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
 
-        AuthenticationService.Instance.SignedIn += () =>
-        {
-            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
-        };
-
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    async void Start()
+    {
+        await Authenticate();
     }
 
     private void Update()
     {
-        HandleLobbyHeartbeat();
+        HandleHeartbeatAsync();
+        HandlePollForUpdatesAsync();
     }
 
-    public async void CreateRelay()
+    async Task Authenticate()
+    {
+        await Authenticate("Player" + Random.Range(0, 1000));
+    }
+
+    async Task Authenticate(string playerName)
+    {
+        if (UnityServices.State == ServicesInitializationState.Uninitialized)
+        {
+            InitializationOptions options = new InitializationOptions();
+            options.SetProfile(playerName);
+
+            await UnityServices.InitializeAsync(options);
+        }
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in as " + AuthenticationService.Instance.PlayerId);
+        };
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            PlayerId = AuthenticationService.Instance.PlayerId;
+            PlayerName = playerName;
+        }
+    }
+
+    public async Task CreateLobby()
+    {
+        if (currentLobby != null) return;
+        try
+        {
+            CurrentHostId = PlayerId;
+            
+            Allocation allocation = await AllocateRelay();
+            string relayJoinCode = await GetRelayJoinCode(allocation);
+
+            CreateLobbyOptions options = new CreateLobbyOptions
+            {
+                IsPrivate = false
+            };
+
+            currentLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, options);
+            Debug.Log("Created lobby: " + currentLobby.Name + " with code " + currentLobby.LobbyCode);
+
+            await LobbyService.Instance.UpdateLobbyAsync(currentLobby.Id, new UpdateLobbyOptions
+            {
+                Data = new Dictionary<string, DataObject>
+                {
+                    { k_keyJoinCode, new DataObject(DataObject.VisibilityOptions.Member, relayJoinCode) },
+                    { k_hostID, new DataObject(DataObject.VisibilityOptions.Member, PlayerId) }
+                }
+            });
+
+            _networkManager.GetComponent<FishyUnityTransport>()
+                .SetRelayServerData(new RelayServerData(allocation, connectionType));
+            
+            Debug.Log("INVOKING EVENT HANDLER");
+
+            OnJoinLobby?.Invoke(this, new LobbyEventArgs { lobby = currentLobby });
+
+            _networkManager.ServerManager.StartConnection();
+            _networkManager.ClientManager.StartConnection();
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError("Failed to create lobby: " + e.Message);
+        }
+    }
+
+
+    public async Task JoinLobby(string joinCode)
+    {
+        if (currentLobby != null) return;
+        try
+        {
+            currentLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(joinCode);
+
+            string relayJoinCode = currentLobby.Data[k_keyJoinCode].Value;
+            JoinAllocation joinAllocation = await JoinRelay(relayJoinCode);
+
+            CurrentHostId = currentLobby.Data[k_keyJoinCode].Value;
+
+            _networkManager.GetComponent<FishyUnityTransport>()
+                .SetRelayServerData(new RelayServerData(joinAllocation, connectionType));
+            
+            OnJoinLobby?.Invoke(this, new LobbyEventArgs { lobby = currentLobby });
+
+            _networkManager.ClientManager.StartConnection();
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError("Failed to quick join lobby: " + e.Message);
+        }
+    }
+
+    public async Task QuickJoinLobby()
+    {
+        if (currentLobby != null) return;
+        try
+        {
+            currentLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+            CurrentHostId = currentLobby.HostId;
+
+            string relayJoinCode = currentLobby.Data[k_keyJoinCode].Value;
+            JoinAllocation joinAllocation = await JoinRelay(relayJoinCode);
+
+            _networkManager.GetComponent<FishyUnityTransport>()
+                .SetRelayServerData(new RelayServerData(joinAllocation, connectionType));
+            
+            OnJoinLobby?.Invoke(this, new LobbyEventArgs { lobby = currentLobby });
+
+            _networkManager.ClientManager.StartConnection();
+
+            Debug.Log(_networkManager.IsServerStarted);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError("Failed to quick join lobby: " + e.Message);
+        }
+    }
+
+    public async Task LeaveLobby()
+    {
+        if (currentLobby == null) return;
+        try
+        {
+            await LobbyService.Instance.RemovePlayerAsync(currentLobby.Id, PlayerId);
+            Debug.Log("Player left the lobby: " + currentLobby.Name);
+
+            currentLobby = null;
+            isHost = false;
+            CurrentHostId = null;
+
+            _networkManager.ClientManager.StopConnection();
+            
+            OnLeaveLobby?.Invoke(this, new LobbyEventArgs { lobby = currentLobby });
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError("Failed to leave lobby: " + e.Message);
+        }
+    }
+
+    [ContextMenu("Get Lobby Info")]
+    public async Task GetLobbyInfo()
+    {
+        if (currentLobby == null)
+        {
+            Debug.Log("Haven't joined bobby");
+        }
+        else
+        {
+            Debug.Log(currentLobby.Players.Count + " player(s) in lobby: " + currentLobby.Name);
+        }
+    }
+
+    async Task<Allocation> AllocateRelay()
     {
         try
         {
-            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);
-            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-            Debug.Log(joinCode);
-
-            _networkManager.GetComponent<FishyUnityTransport>().SetHostRelayData(
-                allocation.RelayServer.IpV4,
-                (ushort)allocation.RelayServer.Port,
-                allocation.AllocationIdBytes,
-                allocation.Key,
-                allocation.ConnectionData
-            );
-
-            _networkManager.GetComponent<NetworkManager>().ServerManager.StartConnection();
-            _networkManager.GetComponent<NetworkManager>().ClientManager.StartConnection();
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxPlayers - 1);
+            return allocation;
         }
         catch (RelayServiceException e)
         {
-            Debug.LogWarning(e);
+            Debug.LogError("Failed to allocate relay: " + e.Message);
+            return default;
         }
     }
 
-    public async void JoinRelay(string joinCode)
+    async Task<string> GetRelayJoinCode(Allocation allocation)
     {
         try
         {
-            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-
-            _networkManager.GetComponent<FishyUnityTransport>().SetClientRelayData(
-                joinAllocation.RelayServer.IpV4,
-                (ushort)joinAllocation.RelayServer.Port,
-                joinAllocation.AllocationIdBytes,
-                joinAllocation.Key,
-                joinAllocation.ConnectionData,
-                joinAllocation.HostConnectionData
-            );
-
-            _networkManager.GetComponent<NetworkManager>().ClientManager.StartConnection();
+            string relayJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            return relayJoinCode;
         }
         catch (RelayServiceException e)
         {
-            Debug.LogWarning(e);
+            Debug.LogError("Failed to get relay join code: " + e.Message);
+            return default;
         }
     }
 
-    private async void HandleLobbyHeartbeat()
-    {
-        if (hostLobby != null)
-        {
-            heartBeatTimer -= Time.deltaTime;
-            if (heartBeatTimer < 0f)
-            {
-                float heartBeatTimerMax = 15;
-                heartBeatTimer = heartBeatTimerMax;
-
-                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
-            }
-        }
-    }
-
-    [ContextMenu("Create Lobby")]
-    private async void CreateLobby()
+    async Task<JoinAllocation> JoinRelay(string relayJoinCode)
     {
         try
         {
-            string lobbyName = "Lobby";
-            int maxPlayers = 2;
-
-            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
-
-            hostLobby = lobby;
-
-            Debug.Log("Create Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(relayJoinCode);
+            return joinAllocation;
         }
-        catch (LobbyServiceException e)
+        catch (RelayServiceException e)
         {
-            Debug.LogWarning(e);
+            Debug.LogError("Failed to join relay: " + e.Message);
+            return default;
         }
     }
 
-    [ContextMenu("List Lobbies")]
-    private async void ListLobbies()
+    private float heartBeatTimer, pollUpdateTimer;
+
+    async Task HandleHeartbeatAsync()
     {
         try
         {
-            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
-
-            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
-
-            foreach (Lobby lobby in queryResponse.Results)
+            if (currentLobby != null && isHost)
             {
-                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+                heartBeatTimer -= Time.deltaTime;
+                if (heartBeatTimer < 0)
+                {
+                    heartBeatTimer = k_lobbyHeartbeatInterval;
+                    await LobbyService.Instance.SendHeartbeatPingAsync(currentLobby.Id);
+                    Debug.Log("Sent heartbeat");
+                }
             }
         }
         catch (LobbyServiceException e)
         {
-            Debug.LogWarning(e);
+            Debug.LogError("Failed to heartbeat lobby: " + e.Message);
         }
     }
 
-    private async void JoinLobby()
+    async Task HandlePollForUpdatesAsync()
     {
         try
         {
-            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+            if (currentLobby != null)
+            {
+                pollUpdateTimer -= Time.deltaTime;
+                if (pollUpdateTimer < 0)
+                {
+                    pollUpdateTimer = k_lobbyPollInterval;
+                    currentLobby = await LobbyService.Instance.GetLobbyAsync(currentLobby.Id);
 
-            await Lobbies.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+                    if (CurrentHostId != currentLobby.HostId)
+                    {
+                        Debug.Log("Host left");
+                        Debug.Log(_networkManager.IsServerStarted);
+                        LeaveLobby();
+                    }
+                }
+            }
         }
         catch (LobbyServiceException e)
         {
-            Debug.LogWarning(e);
+            Debug.LogError("Failed to poll for updates on lobby: " + e.Message);
         }
+    }
+
+    public string getLobbyName()
+    {
+        if (currentLobby == null)
+        {
+            return "null";
+        }
+
+        return currentLobby.Name;
     }
 }
